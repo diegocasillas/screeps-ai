eb2ab7044f52efedafa67e508a4e41fabecc5e79
diegocasillas <diegocasillasdev@gmail.com>
2019-06-29 22:20:04 +0300

    Remove type definition for declared variables

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ErrorMapper } from 'utils/ErrorMapper';
 import Harvester from './roles/Harvester';
 import Upgrader from './roles/Upgrader';
 
-const deleteMissingCreeps = () => {
+const deleteMissingCreeps = (): void => {
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
       delete Memory.creeps[name];
@@ -14,17 +14,19 @@ export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
   deleteMissingCreeps();
 
-  const spawn: StructureSpawn = Game.spawns.Spawn1;
+  const spawn = Game.spawns.Spawn1;
 
-  const harvesters: Harvester[] = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'HARVESTER').map(
-    (creep: Creep) => new Harvester(creep)
-  );
-  const harvestersMinAmount: number = 3;
+  const harvesters = _.filter(
+    Game.creeps,
+    (creep: Creep) => creep.memory.role === 'HARVESTER'
+  ).map((creep: Creep) => new Harvester(creep));
+  const harvestersMinAmount = 3;
 
-  const upgraders: Upgrader[] = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'UPGRADER').map(
-    (creep: Creep) => new Upgrader(creep)
-  );
-  const upgradersMinAmount: number = 3;
+  const upgraders = _.filter(
+    Game.creeps,
+    (creep: Creep) => creep.memory.role === 'UPGRADER'
+  ).map((creep: Creep) => new Upgrader(creep));
+  const upgradersMinAmount = 3;
 
   if (harvesters.length < harvestersMinAmount) {
     spawn.spawnCreep([WORK, MOVE, CARRY], `harvester#${Game.time}`, {
