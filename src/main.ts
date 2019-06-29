0839170f9c86fe59f9fab93c3c35156645f1e590
diegocasillas <diegocasillasdev@gmail.com>
2019-06-29 14:52:13 +0300

    Upgrade room controller with 3 upgraders

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ErrorMapper } from 'utils/ErrorMapper';
 import Harvester from './roles/Harvester';
+import Upgrader from './roles/Upgrader';
 
 const deleteMissingCreeps = () => {
   for (const name in Memory.creeps) {
@@ -15,10 +16,15 @@ export const loop = ErrorMapper.wrapLoop(() => {
 
   const spawn: StructureSpawn = Game.spawns.Spawn1;
 
-  const harvesters = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'HARVESTER').map(
+  const harvesters: Harvester[] = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'HARVESTER').map(
     (creep: Creep) => new Harvester(creep)
   );
-  const harvestersMinAmount = 3;
+  const harvestersMinAmount: number = 3;
+
+  const upgraders: Upgrader[] = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'UPGRADER').map(
+    (creep: Creep) => new Upgrader(creep)
+  );
+  const upgradersMinAmount: number = 3;
 
   if (harvesters.length < harvestersMinAmount) {
     spawn.spawnCreep([WORK, MOVE, CARRY], `harvester#${Game.time}`, {
@@ -26,5 +32,12 @@ export const loop = ErrorMapper.wrapLoop(() => {
     });
   }
 
+  if (upgraders.length < upgradersMinAmount) {
+    spawn.spawnCreep([WORK, MOVE, CARRY], `upgrader#${Game.time}`, {
+      memory: { role: 'UPGRADER', room: spawn.room.name }
+    });
+  }
+
   harvesters.forEach((harvester: Harvester) => harvester.run());
+  upgraders.forEach((upgrader: Upgrader) => upgrader.run());
 });
