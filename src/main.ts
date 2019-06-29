28eef4f5a33b23d69e050f8ce19d2cc8811bb99b
diegocasillas <diegocasillasdev@gmail.com>
2019-06-29 14:25:44 +0300

    Harvest closest source with 3 harvesters

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ErrorMapper } from 'utils/ErrorMapper';
+import Harvester from './roles/Harvester';
 
 const deleteMissingCreeps = () => {
   for (const name in Memory.creeps) {
@@ -11,4 +12,19 @@ const deleteMissingCreeps = () => {
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
   deleteMissingCreeps();
+
+  const spawn: StructureSpawn = Game.spawns.Spawn1;
+
+  const harvesters = _.filter(Game.creeps, (creep: Creep) => creep.memory.role === 'HARVESTER').map(
+    (creep: Creep) => new Harvester(creep)
+  );
+  const harvestersMinAmount = 3;
+
+  if (harvesters.length < harvestersMinAmount) {
+    spawn.spawnCreep([WORK, MOVE, CARRY], `harvester#${Game.time}`, {
+      memory: { role: 'HARVESTER', room: spawn.room.name }
+    });
+  }
+
+  harvesters.forEach((harvester: Harvester) => harvester.run());
 });
