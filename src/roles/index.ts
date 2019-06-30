bbde7cff725b50953e048c5ba1c9e026aa8024b2
diegocasillas <diegocasillasdev@gmail.com>
2019-06-30 19:49:32 +0300

    Create role list

---
+++ src/roles/index.ts
@@ -0,0 +1,36 @@
+import Harvester from './Harvester';
+import Hauler from './Hauler';
+import Upgrader from './Upgrader';
+
+export const roleList: any = {
+  HARVESTER: {
+    model: Harvester,
+    min: 3,
+    spawnProperties: {
+      body: [WORK, CARRY, MOVE],
+      name: 'harvester',
+      role: 'HARVESTER',
+      state: { hauler: null }
+    }
+  },
+  HAULER: {
+    model: Hauler,
+    min: 3,
+    spawnProperties: {
+      body: [MOVE, MOVE, MOVE, CARRY, CARRY, CARRY],
+      name: 'hauler',
+      role: 'HAULER',
+      state: { harvester: null }
+    }
+  },
+  UPGRADER: {
+    model: Upgrader,
+    min: 3,
+    spawnProperties: {
+      body: [WORK, CARRY, MOVE],
+      name: 'upgrader',
+      role: 'UPGRADER',
+      state: {}
+    }
+  }
+};
